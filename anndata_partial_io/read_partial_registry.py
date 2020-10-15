9625b8cf329a7fcdf45c59e555ab1d3c4f9700af
Isaac Virshup <ivirshup@gmail.com>
2020-10-15 19:56:58 +1100

    Reorganize

--- anndata_partial_io/read_partial_registry.py
+++ anndata_partial_io/read_partial_registry.py
@@ -16,18 +16,8 @@ from anndata import AnnData
 from anndata._core.index import _normalize_indices
 from anndata._core.merge import intersect_keys
 from anndata._core.sparse_dataset import SparseDataset
+from anndata._core import views
 from scipy.sparse import data
-
-
-def _to_hdf5_vlen_strings(value: np.ndarray) -> np.ndarray:
-    """This corrects compound dtypes to work with hdf5 files."""
-    new_dtype = []
-    for dt_name, (dt_type, _) in value.dtype.fields.items():
-        if dt_type.kind in ("U", "O"):
-            new_dtype.append((dt_name, h5py.special_dtype(vlen=str)))
-        else:
-            new_dtype.append((dt_name, dt_type))
-    return value.astype(new_dtype)
 
 
 # TODO: This probably should be replaced by a hashable Mapping due to conversion b/w "_" and "-"
@@ -106,80 +96,38 @@ def get_schema(elem: Union[h5py.Dataset, h5py.Group]) -> IOSpec:
     )
 
 
+####################
+# Dispatch methods #
+####################
+
+
 def read_elem(elem):
+    """Read an element from an on disk store."""
     return _REGISTRY.read[get_schema(elem)](elem)
 
 
 def read_elem_partial(elem, *, items=None, indices=(slice(None), slice(None))):
+    """Read part of an element from an on disk store."""
     return _REGISTRY.read_partial[get_schema(elem)](elem, items=items, indices=indices)
 
 
-@_REGISTRY.register_read({"encoding-type": "array", "encoding-version": "0.2.0"})
-def read_array(elem):
-    return elem[()]
-
-
-@_REGISTRY.register_read_partial(
-    {"encoding-type": "array", "encoding-version": "0.2.0"}
-)
-def read_array_partial(elem, *, items=None, indices=(slice(None, None))):
-    return elem[indices]
-
-
-@_REGISTRY.register_read({"encoding-type": "csc_matrix", "encoding-version": "0.1.0"})
-@_REGISTRY.register_read({"encoding-type": "csr_matrix", "encoding-version": "0.1.0"})
-def read_sparse(elem):
-    return SparseDataset(elem).to_memory()
-
-
-@_REGISTRY.register_read_partial(
-    {"encoding-type": "csc_matrix", "encoding-version": "0.1.0"}
-)
-@_REGISTRY.register_read_partial(
-    {"encoding-type": "csr_matrix", "encoding-version": "0.1.0"}
-)
-def read_sparse_partial(elem, *, items=None, indices=(slice(None), slice(None))):
-    return SparseDataset(elem)[indices]
-
-
-@_REGISTRY.register_read({"encoding-type": "dataframe", "encoding-version": "0.1.0"})
-def read_dataframe_0_1_0(elem):
-    columns = list(elem.attrs["column-order"])
-    idx_key = elem.attrs["_index"]
-    df = pd.DataFrame(
-        {k: read_series(elem[k]) for k in columns},
-        index=read_series(elem[idx_key]),
-        columns=list(columns),
-    )
-    if idx_key != "_index":
-        df.index.name = idx_key
-    return df
-
-
-def read_series(dataset) -> Union[np.ndarray, pd.Categorical]:
-    # For reading older dataframes
-    if "categories" in dataset.attrs:
-        categories_dset = dataset.parent[dataset.attrs["categories"]]
-        categories = categories_dset[...]
-        ordered = bool(categories_dset.attrs.get("ordered", False))
-        return pd.Categorical.from_codes(dataset[...], categories, ordered=ordered)
+def write_elem(f: h5py.Group, k: str, elem, *args, **kwargs):
+    """Write an element to an on disk store."""
+    t = type(elem)
+    if hasattr(elem, "dtype") and (t, elem.dtype.kind) in _REGISTRY.write:
+        _REGISTRY.write[(t, elem.dtype.kind)](f, k, elem, *args, **kwargs)
     else:
-        return dataset[...]
+        _REGISTRY.write[t](f, k, elem, *args, **kwargs)
 
 
-@_REGISTRY.register_read_partial(
-    {"encoding-type": "dataframe", "encoding-version": "0.1.0"}
-)
-def read_partial_dataframe_0_1_0(
-    elem, *, items=None, indices=(slice(None), slice(None))
-):
-    if items is None:
-        items = slice(None)
-    else:
-        items = list(items)
-    return read_elem(elem)[items].iloc[indices[0]]
+################################
+# Fallbacks / backwards compat #
+################################
 
-@_REGISTRY.register_read({"encoding-type": "", "encoding-version": ""})
+# Note: there is no need for writing in a backwards compatible format
+
+
+@_REGISTRY.register_read(IOSpec("", ""))
 def read_basic(elem):
     from anndata._io import h5ad
 
@@ -189,7 +137,7 @@ def read_basic(elem):
         return h5ad.read_dataset(elem)  # TODO: Handle legacy
 
 
-@_REGISTRY.register_read_partial({"encoding-type": "", "encoding-version": ""})
+@_REGISTRY.register_read_partial(IOSpec("", ""))
 def read_basic_partial(elem, *, items=None, indices=(slice(None), slice(None))):
     if isinstance(elem, Mapping):
         return _read_partial(elem, items=items, indices=indices)
@@ -199,21 +147,9 @@ def read_basic_partial(elem, *, items=None, indices=(slice(None), slice(None))):
         return elem[()]
 
 
-def _read_partial(group, *, items=None, indices=(slice(None), slice(None))):
-    if group is None:
-        return None
-    if items is None:
-        keys = intersect_keys((group,))
-    else:
-        keys = intersect_keys((group, items))
-    result = {}
-    for k in keys:
-        if isinstance(items, Mapping):
-            next_items = items.get(k, None)
-        else:
-            next_items = None
-        result[k] = read_elem_partial(group[k], items=next_items, indices=indices)
-    return result
+###########
+# AnnData #
+###########
 
 
 def read_indices(group):
@@ -279,6 +215,24 @@ def read_partial(
 
     return ad.AnnData(**result)
 
+
+def _read_partial(group, *, items=None, indices=(slice(None), slice(None))):
+    if group is None:
+        return None
+    if items is None:
+        keys = intersect_keys((group,))
+    else:
+        keys = intersect_keys((group, items))
+    result = {}
+    for k in keys:
+        if isinstance(items, Mapping):
+            next_items = items.get(k, None)
+        else:
+            next_items = None
+        result[k] = read_elem_partial(group[k], items=next_items, indices=indices)
+    return result
+
+
 def read(pth):
     with h5py.File(pth, "r") as f:
         results = {k: read_elem(v) for k, v in f.items()}
@@ -288,25 +242,6 @@ def read(pth):
 def write(adata, pth, dataset_kwargs=MappingProxyType({})):
     with h5py.File(pth, "w") as f:
         write_elem(f, "/", adata, dataset_kwargs=dataset_kwargs)
-
-
-def write_elem(f: h5py.Group, k: str, elem, *args, **kwargs):
-    t = type(elem)
-    if hasattr(elem, "dtype") and (t, elem.dtype.kind) in _REGISTRY.write:
-        _REGISTRY.write[(t, elem.dtype.kind)](f, k, elem, *args, **kwargs)
-    else:
-        _REGISTRY.write[t](f, k, elem, *args, **kwargs)
-
-@_REGISTRY.register_read(IOSpec("dict", "0.1.0"))
-def read_mapping(elem):
-    return {k: read_elem(v) for k, v in elem.items()}
-
-
-@_REGISTRY.register_write(dict, IOSpec("dict", "0.1.0"))
-def write_mapping(f, k, v, dataset_kwargs=MappingProxyType({})):
-    g = f.create_group(k)
-    for sub_k, sub_v in v.items():
-        write_elem(g, sub_k, sub_v, dataset_kwargs=dataset_kwargs)
 
 
 @_REGISTRY.register_write(AnnData, IOSpec("anndata", "0.1.0"))
@@ -322,26 +257,86 @@ def write_anndata(f, k, adata, dataset_kwargs=MappingProxyType({})):
     write_elem(f, "uns", dict(adata.uns), dataset_kwargs=dataset_kwargs)
 
 
+############
+# Mappings #
+############
+
+
+@_REGISTRY.register_read(IOSpec("dict", "0.1.0"))
+def read_mapping(elem):
+    return {k: read_elem(v) for k, v in elem.items()}
+
+
+@_REGISTRY.register_write(dict, IOSpec("dict", "0.1.0"))
+def write_mapping(f, k, v, dataset_kwargs=MappingProxyType({})):
+    g = f.create_group(k)
+    for sub_k, sub_v in v.items():
+        write_elem(g, sub_k, sub_v, dataset_kwargs=dataset_kwargs)
+
+
+##############
+# np.ndarray #
+##############
+
+
+@_REGISTRY.register_write(views.ArrayView, IOSpec("array", "0.2.0"))
 @_REGISTRY.register_write(np.ndarray, IOSpec("array", "0.2.0"))
 def write_basic(f, k, elem, dataset_kwargs=MappingProxyType({})):
     """Write methods which underlying library handles nativley."""
     f.create_dataset(k, data=elem, **dataset_kwargs)
 
 
+@_REGISTRY.register_read(IOSpec("array", "0.2.0"))
+def read_array(elem):
+    return elem[()]
+
+
+@_REGISTRY.register_read_partial(IOSpec("array", "0.2.0"))
+def read_array_partial(elem, *, items=None, indices=(slice(None, None))):
+    return elem[indices]
+
+
+# arrays of strings
+
+_REGISTRY.register_read(IOSpec("string-array", "0.2.0"))(read_basic)
+
+
+@_REGISTRY.register_write((views.ArrayView, "U"), IOSpec("string-array", "0.2.0"))
+@_REGISTRY.register_write((views.ArrayView, "O"), IOSpec("string-array", "0.2.0"))
+@_REGISTRY.register_write((np.ndarray, "U"), IOSpec("string-array", "0.2.0"))
+@_REGISTRY.register_write((np.ndarray, "O"), IOSpec("string-array", "0.2.0"))
+def write_vlen_string_array(f, k, elem, dataset_kwargs=MappingProxyType({})):
+    """Write methods which underlying library handles nativley."""
+    f.create_dataset(k, data=elem, dtype=h5py.special_dtype(vlen=str), **dataset_kwargs)
+
+
+###############
+# np.recarray #
+###############
+
+
+def _to_hdf5_vlen_strings(value: np.ndarray) -> np.ndarray:
+    """This corrects compound dtypes to work with hdf5 files."""
+    new_dtype = []
+    for dt_name, (dt_type, _) in value.dtype.fields.items():
+        if dt_type.kind in ("U", "O"):
+            new_dtype.append((dt_name, h5py.special_dtype(vlen=str)))
+        else:
+            new_dtype.append((dt_name, dt_type))
+    return value.astype(new_dtype)
+
+
 _REGISTRY.register_read(IOSpec("rec-array", "0.2.0"))(read_basic)
+
 
 @_REGISTRY.register_write(np.recarray, IOSpec("rec-array", "0.2.0"))
 def write_recarray(f, k, elem, dataset_kwargs=MappingProxyType({})):
     f.create_dataset(k, data=_to_hdf5_vlen_strings(elem), **dataset_kwargs)
 
 
-_REGISTRY.register_read(IOSpec("string-array", "0.2.0"))(read_basic)
-
-@_REGISTRY.register_write((np.ndarray, "U"), IOSpec("string-array", "0.2.0"))
-@_REGISTRY.register_write((np.ndarray, "O"), IOSpec("string-array", "0.2.0"))
-def write_vlen_string_array(f, k, elem, dataset_kwargs=MappingProxyType({})):
-    """Write methods which underlying library handles nativley."""
-    f.create_dataset(k, data=elem, dtype=h5py.special_dtype(vlen=str), **dataset_kwargs)
+#################
+# Sparse arrays #
+#################
 
 
 def write_sparse_compressed(
@@ -362,9 +357,29 @@ def write_sparse_compressed(
 write_csr = partial(write_sparse_compressed, fmt="csr")
 write_csc = partial(write_sparse_compressed, fmt="csc")
 _REGISTRY.register_write(sparse.csr_matrix, IOSpec("csr_matrix", "0.1.0"))(write_csr)
+_REGISTRY.register_write(views.SparseCSRView, IOSpec("csr_matrix", "0.1.0"))(write_csr)
 _REGISTRY.register_write(sparse.csc_matrix, IOSpec("csc_matrix", "0.1.0"))(write_csc)
+_REGISTRY.register_write(views.SparseCSCView, IOSpec("csc_matrix", "0.1.0"))(write_csc)
 
 
+@_REGISTRY.register_read(IOSpec("csc_matrix", "0.1.0"))
+@_REGISTRY.register_read(IOSpec("csr_matrix", "0.1.0"))
+def read_sparse(elem):
+    return SparseDataset(elem).to_memory()
+
+
+@_REGISTRY.register_read_partial(IOSpec("csc_matrix", "0.1.0"))
+@_REGISTRY.register_read_partial(IOSpec("csr_matrix", "0.1.0"))
+def read_sparse_partial(elem, *, items=None, indices=(slice(None), slice(None))):
+    return SparseDataset(elem)[indices]
+
+
+##############
+# DataFrames #
+##############
+
+
+@_REGISTRY.register_write(views.DataFrameView, IOSpec("dataframe", "0.2.0"))
 @_REGISTRY.register_write(pd.DataFrame, IOSpec("dataframe", "0.2.0"))
 def write_dataframe(f, key, df, dataset_kwargs=MappingProxyType({})):
     # Check arguments
@@ -403,6 +418,50 @@ def read_dataframe(elem):
     return df
 
 
+# Backwards compat dataframe reading
+
+
+@_REGISTRY.register_read({"encoding-type": "dataframe", "encoding-version": "0.1.0"})
+def read_dataframe_0_1_0(elem):
+    columns = list(elem.attrs["column-order"])
+    idx_key = elem.attrs["_index"]
+    df = pd.DataFrame(
+        {k: read_series(elem[k]) for k in columns},
+        index=read_series(elem[idx_key]),
+        columns=list(columns),
+    )
+    if idx_key != "_index":
+        df.index.name = idx_key
+    return df
+
+
+def read_series(dataset) -> Union[np.ndarray, pd.Categorical]:
+    # For reading older dataframes
+    if "categories" in dataset.attrs:
+        categories_dset = dataset.parent[dataset.attrs["categories"]]
+        categories = categories_dset[...]
+        ordered = bool(categories_dset.attrs.get("ordered", False))
+        return pd.Categorical.from_codes(dataset[...], categories, ordered=ordered)
+    else:
+        return dataset[...]
+
+
+@_REGISTRY.register_read_partial(IOSpec("dataframe", "0.1.0"))
+def read_partial_dataframe_0_1_0(
+    elem, *, items=None, indices=(slice(None), slice(None))
+):
+    if items is None:
+        items = slice(None)
+    else:
+        items = list(items)
+    return read_elem(elem)[items].iloc[indices[0]]
+
+
+###############
+# Categorical #
+###############
+
+
 @_REGISTRY.register_write(pd.Categorical, IOSpec("categorical", "0.2.0"))
 def write_categorical(f, k, v, dataset_kwargs=MappingProxyType({})):
     g = f.create_group(k)
@@ -421,6 +480,11 @@ def read_categorical(elem):
     )
 
 
+###########
+# Scalars #
+###########
+
+
 @_REGISTRY.register_read(IOSpec("numeric-scalar", "0.2.0"))
 def read_scalar(elem):
     return elem[()]
@@ -434,16 +498,20 @@ def write_numeric_scalar(f, key, value, dataset_kwargs=MappingProxyType({})):
     f.create_dataset(key, data=np.array(value), **dataset_kwargs)
 
 
-for numeric_scalar_type in  [
+# fmt: off
+for numeric_scalar_type in [
     bool, np.bool_,
     np.uint8, np.uint16, np.uint32, np.uint64,
     int, np.int8, np.int16, np.int32, np.int64,
     float, np.float16, np.float32, np.float64, np.float128,
     np.complex64, np.complex128, np.complex256,
 ]:
+# fmt: on
     _REGISTRY.register_write(numeric_scalar_type, IOSpec("numeric-scalar", "0.2.0"))(write_numeric_scalar)
 
+
 _REGISTRY.register_read(IOSpec("string", "0.2.0"))(read_scalar)
+
 
 @_REGISTRY.register_write(np.str_, IOSpec("string", "0.2.0"))
 @_REGISTRY.register_write(str, IOSpec("string", "0.2.0"))
@@ -451,4 +519,6 @@ def write_string(f, k, v, dataset_kwargs):
     if "compression" in dataset_kwargs:
         dataset_kwargs = dict(dataset_kwargs)
         dataset_kwargs.pop("compression")
-    f.create_dataset(k, data=np.array(v, dtype=h5py.string_dtype(encoding='utf-8')), **dataset_kwargs)
+    f.create_dataset(
+        k, data=np.array(v, dtype=h5py.string_dtype(encoding="utf-8")), **dataset_kwargs
+    )
